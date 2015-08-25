27621f6e8a70f7c7559cd4779a8fb4dd4d9dc865
LU MINGBEI <mingbei.lu@linecorp.com>
2015-08-25 16:38:42 +0900

    add Coroutine Extensions

---
+++ UnityExtensions/Assets/Scripts/Coroutine/CoroutineExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class CoroutineExtensions {
+	public static Coroutine StartCoroutineEx(this MonoBehaviour monoBehaviour, IEnumerator routine, out CoroutineController coroutineController) {
+		if (routine == null) {
+			throw new System.ArgumentNullException("routine is NULL");
+		}
+		coroutineController = new CoroutineController(routine);
+		return monoBehaviour.StartCoroutine(coroutineController.Start());
+	}
+}
