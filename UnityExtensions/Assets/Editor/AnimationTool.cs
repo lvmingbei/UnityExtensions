3867be36d8bc8dba5d22c8ae80240effa29d0264
LU MINGBEI <mingbei.lu@linecorp.com>
2015-12-03 16:08:39 +0900

    animation editor tool

---
+++ UnityExtensions/Assets/Editor/AnimationTool.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Collections;
+
+namespace UnityEditor
+{
+	public class AnimationTool
+	{
+		[MenuItem ("Designer Tools/Extract Animation")]
+		public static void RenameMotion ()
+		{	
+			foreach (UnityEngine.Object obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets)) {
+				AnimationClip orgClip = (AnimationClip)AssetDatabase.LoadAssetAtPath (AssetDatabase.GetAssetPath (obj), typeof(AnimationClip));
+				if (orgClip == null)
+					continue;
+				AnimationClip placeClip = new AnimationClip ();
+				EditorUtility.CopySerialized (orgClip, placeClip);
+				AssetDatabase.CreateAsset (placeClip, AssetDatabase.GetAssetPath (obj).Replace (".fbx", ".anim"));
+				AssetDatabase.Refresh ();
+			}
+		}
+
+		[MenuItem ("Designer Tools/Extract Animation(delete fbx file)")]
+		public static void RenameMotionDelFbx ()
+		{	
+			foreach (UnityEngine.Object obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets)) {
+				AnimationClip orgClip = (AnimationClip)AssetDatabase.LoadAssetAtPath (AssetDatabase.GetAssetPath (obj), typeof(AnimationClip));
+				if (orgClip == null)
+					continue;
+				AnimationClip placeClip = new AnimationClip ();
+				EditorUtility.CopySerialized (orgClip, placeClip);
+				AssetDatabase.CreateAsset (placeClip, AssetDatabase.GetAssetPath (obj).Replace (".fbx", ".anim"));
+				AssetDatabase.DeleteAsset (AssetDatabase.GetAssetPath (obj));
+				AssetDatabase.Refresh ();
+			}
+		}
+	}
+}
